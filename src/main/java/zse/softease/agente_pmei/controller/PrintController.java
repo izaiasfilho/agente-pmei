a26b3d1087f921aaa1f3c136adb6aa97952b46b9
izaiasfilho <izs.af@hotmail.com>
2026-01-25 08:01:32 -0300

    comunicaçao direto para o controller back original

--- src/main/java/zse/softease/agente_pmei/controller/PrintController.java
+++ src/main/java/zse/softease/agente_pmei/controller/PrintController.java
@@ -5,20 +5,18 @@ package zse.softease.agente_pmei.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import zse.softease.agente_pmei.service.ExecutorImpressaoService;
-
 @RestController
 public class PrintController {
 
-    private final ExecutorImpressaoService printService;
+  //  private final ExecutorImpressaoService printService;
 
-    public PrintController(ExecutorImpressaoService printService) {
-        this.printService = printService;
+    public PrintController() {
+       // this.printService = printService;
     }
 
     @GetMapping("/print/test")
     public String printTest() {
-    	printService.printTest();
+    	//printService.printTest();
         return "Teste enviado para impressora";
     }
 }
